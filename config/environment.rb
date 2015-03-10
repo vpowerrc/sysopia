a4d1de23d73bcc2b5f04c9e87f5e804035a6c8a8
Viduranga Wijesooriya <vpowerrc@gmail.com>
2015-03-11 02:16:48 +0530

    modified how database configs are loaded
    Signed-off-by: Viduranga Wijesooriya <vpowerrc@gmail.com>

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,7 @@ require "ostruct"
 require "yaml"
 require "active_record"
 require "fileutils"
+require 'json'
 
 # All encompassing module for the project
 module Sysopia
@@ -25,17 +26,7 @@ module Sysopia
   end
 
   def self.read_env
-    if ENV["SYSOPIA"]
-      open(File.join(__dir__, ENV["SYSOPIA"])).each do |l|
-        key, val = l.strip.split("=")
-        ENV[key] = val if key && val
-      end
-    end
-    self.check_env
-  end
-
-  def self.check_env
-    e_required = open(File.join(__dir__, "config", "env.sh")).map do |l|
+    e_required = open(File.join(__dir__, "env.sh")).map do |l|
       key, val = l.strip.split("=")
       val && key
     end.compact
@@ -44,13 +35,30 @@ module Sysopia
     extra = e_real - e_required
     raise("Missing env variables: #{missing.join(', ')}") unless missing.empty?
     raise("Extra env variables: #{extra.join(', ')}") unless extra.empty?
+
+    return {
+      "database" => { env.to_s => { 'host' => ENV["SYSOPIA_DB_HOST"], 'username' => ENV["SYSOPIA_DB_USERNAME"], 'password' => ENV["SYSOPIA_DB_PASSWORD"] } },
+      "session_secret" => ENV["SYSOPIA_SESSION_SECRET"],
+      "timezone_offset" => ENV["SYSOPIA_TIMEZONE_OFFSET"]
+    }
   end
 
   private
 
   def self.init_conf
-    raw_conf = File.read(File.join(__dir__, "config", "config.yml"))
+    raw_conf = File.read(File.join(__dir__, "db_config.yml"))
     conf = YAML.load(ERB.new(raw_conf).result)
+
+    #read configurations from system environment variables
+    if ENV["SYSOPIA_DB_HOST"]
+      conf_new = read_env
+      conf_db = conf_new["database"][env.to_s]
+      conf_db = conf["database"][env.to_s].merge(conf_new["database"][env.to_s]) do |k, old_v, new_v|
+        new_v
+      end
+      conf["database"][env.to_s] = conf_db
+    end
+
     OpenStruct.new(
       session_secret:   conf["session_secret"],
       database:         conf["database"],
@@ -59,10 +67,4 @@ module Sysopia
   end
 end
 
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), "models"))
-Dir.glob(File.join(File.dirname(__FILE__), "models", "**", "*.rb")) do |app|
-  require File.basename(app, ".*")
-end
-
-Sysopia.read_env
 Sysopia.db_connection
